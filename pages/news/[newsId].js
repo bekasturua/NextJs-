9a9daae5b7464b4a8a2f5649f012c0966f85d62c
Beka Sturua <beqasturua3@gmail.com>
2022-08-13 07:00:22 +0400

    Extracting Dynamic Parameter Values

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,7 +1,16 @@
+import { useRouter } from "next/router";
+
 // our-domain.com/news/something-important
 
 function DetailPage() {
-  return <h1>The News Page</h1>;
+  const router = useRouter();
+
+  const newsId = router.query.newsId;
+
+  // send a request to the backedn API
+  // to getch the news item with newId
+
+  return <h1>The Detail Page</h1>;
 }
 
 export default DetailPage;
